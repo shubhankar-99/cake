ee4ea2790f3310ca0f6b71ce2383a1cde1315e1e
Shubhankar Saxena <saxena03121999@gmail.com>
2020-10-12 00:11:45 +0530

    Updates

---
+++ precache-manifest.821c15e677b49b0ee76c7f56b4219960.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87821eb57c80ee2778417ad13df03ad3",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "ccc8624d2d7cc5cc47bf",
+    "url": "/react-gh-pages/static/css/main.34306f3e.chunk.css"
+  },
+  {
+    "revision": "90fa83605a2ea9d24931",
+    "url": "/react-gh-pages/static/js/2.ce473a44.chunk.js"
+  },
+  {
+    "revision": "f7def617c168a3e9270f21ef8a75c08f",
+    "url": "/react-gh-pages/static/js/2.ce473a44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccc8624d2d7cc5cc47bf",
+    "url": "/react-gh-pages/static/js/main.ab266823.chunk.js"
+  },
+  {
+    "revision": "610152edab3ef0330865",
+    "url": "/react-gh-pages/static/js/runtime-main.ae20fe5a.js"
+  },
+  {
+    "revision": "c029a7b5aa6aefe8b9628fe90c406c27",
+    "url": "/react-gh-pages/static/media/bg.c029a7b5.jpeg"
+  },
+  {
+    "revision": "966dc066c31f000fae8d052dd35c26cb",
+    "url": "/react-gh-pages/static/media/cake.966dc066.jpg"
+  }
+]);
